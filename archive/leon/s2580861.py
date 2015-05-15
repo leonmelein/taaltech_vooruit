04625e9587b3652e914602a4074e57f8b25d2a06
Joost van Eck <joosteck@gmail.com>
2015-05-15 12:13:09 +0200

    ook plain text question file
    met de benodigde naamswijzigingen en een print statement in Leons code,
    zodat je weet welke vraag er gesteld wordt als je vanuit een text file
    uitleest

--- archive/leon/s2580861.py
+++ archive/leon/s2580861.py
@@ -232,6 +232,7 @@ if __name__ == "__main__":
     if len(sys.argv) >= 2:
         with open(sys.argv[1], 'r') as questions:
             for question in questions:
+                print(question)
                 main(question)
 
     # Check standard input
